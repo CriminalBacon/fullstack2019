3da18c46a2eb6aec8fc6d1c0c33c99910e01cb85
MT <mt>
2019-10-01 13:25:59 +0100

    finished 1a

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -1,6 +1,32 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+const Header = (props) => {
+    return (
+        <h1>{props.courseName}</h1>
+
+    )
+}
+
+const Part = (props) => {
+    return (
+        <div>
+            <p>
+            {props.partName} {props.exerciseNumber}
+            </p>
+        </div>
+
+    )
+}
+
+const Total = (props) => {
+    return(
+        <p>Number of exercises {props.sum}</p>
+
+    )
+
+}
+
 const App = () => {
     const course = 'Half Stack application development'
     const part1 = 'Fundementals for React'
@@ -9,20 +35,14 @@ const App = () => {
     const exercises2 = 7
     const part3 = 'State of a component'
     const exercises3 = 14
-
+    
     return (
         <div>
-            <h1>{course}</h1>
-            <p>
-                {part1} {exercises1}
-            </p>
-            <p>
-                {part2} {exercises2}
-            </p>
-            <p>
-                {part3} {exercises3}
-            </p>
-            <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+            <Header courseName={course}/>
+            <Part partName={part1} exerciseNumber={exercises1}/>
+            <Part partName={part2} exerciseNumber={exercises2}/>
+            <Part partName={part3} exerciseNumber={exercises3}/>
+            <Total sum={exercises1 + exercises2 + exercises3}/>
 
         </div>    
     )
