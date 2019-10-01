9489325870794034662c647d25b67fa589bc7cd2
MT <mt>
2019-10-01 16:47:52 +0100

    Update index.js

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -29,20 +29,26 @@ const Total = (props) => {
 
 const App = () => {
     const course = 'Half Stack application development'
-    const part1 = 'Fundementals for React'
-    const exercises1 = 10
-    const part2 = 'Using props to pass data'
-    const exercises2 = 7
-    const part3 = 'State of a component'
-    const exercises3 = 14
+    const part1 = {
+      name: 'Fundamentals of React',
+      exercises: 10
+    }
+    const part2 = {
+      name: 'Using props to pass data',
+      exercises: 7
+    }
+    const part3 = {
+      name: 'State of a component',
+      exercises: 14
+    }
     
     return (
         <div>
             <Header courseName={course}/>
-            <Part partName={part1} exerciseNumber={exercises1}/>
-            <Part partName={part2} exerciseNumber={exercises2}/>
-            <Part partName={part3} exerciseNumber={exercises3}/>
-            <Total sum={exercises1 + exercises2 + exercises3}/>
+            <Part partName={part1.name} exerciseNumber={part1.exercises}/>
+            <Part partName={part2.name} exerciseNumber={part2.exercises}/>
+            <Part partName={part3.name} exerciseNumber={part3.exercises}/>
+            <Total sum={part1.exercises + part2.exercises + part3.exercises}/>
 
         </div>    
     )
