85281bd63452d0bf27c71abc021b638f1f89af11
MT <mt>
2019-10-05 12:57:08 +0100

    completed 1.6

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -1,12 +1,64 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { useState } from 'react'
+import ReactDom from 'react-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Header = ({ heading }) => (
+    <p>
+        <h1>
+        {heading}
+        </h1>
+    </p>
+)
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const Button = ({ onClick, text }) => (
+    <button onClick = {onClick}>
+        {text}
+    </button>
+
+)
+
+const Content = ({ label, number }) => (
+    <p>
+        {label} {number}
+    </p>
+
+)
+
+const App = () => {
+    // save clicks of each button to own state
+    const [good, setGood] = useState(0)
+    const [neutral, setNeutral] = useState(0)
+    const [bad, setBad] = useState(0)
+
+    const handleGoodClick = () => {
+        setGood(good + 1)
+
+    }
+
+    const handleNeutralClick = () => {
+        setNeutral(neutral + 1)
+    }
+
+    const handleBadClick = () => {
+        setBad(bad + 1)
+    }
+
+    return (
+        <div>
+            <Header heading="give feedback"/>
+            <Button onClick={handleGoodClick} text='good'/>
+            <Button onClick={handleNeutralClick} text='neutral'/>
+            <Button onClick={handleBadClick} text='bad'/>
+            <Header heading="statistics"/>
+            <p>
+            <Content label='Good' number={good}></Content>
+            <Content label='Neutral' number={neutral}></Content>
+            <Content label='Bad' number={bad}></Content>
+            </p>
+        </div>
+    )
+
+}
+
+ReactDom.render(<App />, 
+    document.getElementById('root')
+)
