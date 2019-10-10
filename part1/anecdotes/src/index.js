55712629b4a6c5ed0a11b9f7854d4b16c47a8026
MT <mt>
2019-10-10 10:37:45 +0100

    finished 1.14

--- part1/anecdotes/src/index.js
+++ part1/anecdotes/src/index.js
@@ -9,10 +9,51 @@ const anecdotes = [
     'Premature optimization is the root of all evil.',
     'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
   ]
-  
+
+const Header = ({ heading }) => (
+    <h1>
+        {heading}
+    </h1>
+)
+
+const Button = ({ onClick, text }) => (
+    <button onClick = {onClick}>
+        {text}
+    </button>
+)
+
+const DisplayAnectdote = ({ toDisplay, count}) => (
+    <p>
+        {toDisplay}
+        <br></br>
+        has {count} votes
+    </p>
+)
+
+const DisplayPopularAnectdote = ({ toDisplay, voteArray, count}) => {
+    if ((Math.max(...voteArray)) === 0) {
+        return( 
+            <p>
+                No votes have been cast.
+            </p>
+        )
+    }
+    return (
+        <p>
+            {toDisplay}
+            <br></br>
+            has {count} votes
+        </p>
+)}
+
+
 const App = (props) => {
     const [selected, setSelected] = useState(0)
     const [votes, setVote] = useState(new Array(anecdotes.length).fill(0))
+    const [popularVoteIndex, setPopularVoteIndex] = useState(0)
+
+    console.log(votes, 'votes')
+    console.log(popularVoteIndex, 'popularvoteindex')
 
     const nextAnectdote = () => {
         const min = 0
@@ -23,26 +64,25 @@ const App = (props) => {
     }
     
     const handleVoteClick = () => {
-
         const copy = [...votes]
         copy[selected] += 1
         setVote(copy)
-
+        setPopularVoteIndex(copy.indexOf(Math.max(...copy)))
     }
-
+ 
     return (
         <div>
-            {props.anecdotes[selected]}
-            <br></br>
-            has {votes[selected]} votes.
+            <Header heading={'Anecdote of the day'}/>
+            <DisplayAnectdote toDisplay={props.anecdotes[selected]} count= {votes[selected]}/>
             <p>
-                <button onClick={() =>handleVoteClick()}>vote</button>
-                <button onClick={() => nextAnectdote()}>next anectode</button>
+                <Button onClick={() => handleVoteClick()} text='vote'/>
+                <Button onClick={() => nextAnectdote()} text='next anectode'/>
             </p>
+            <Header heading={'Anecdote with most votes'}/>
+            <DisplayPopularAnectdote toDisplay={anecdotes[popularVoteIndex]} voteArray={votes} count={votes[popularVoteIndex]}/>
         </div>
         )
     }
-
 
 ReactDOM.render(
   <App anecdotes={anecdotes} />,
